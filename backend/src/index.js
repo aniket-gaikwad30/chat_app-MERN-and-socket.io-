39720a9163b23243c014e955c1688737b3d37eba
aniket-gaikwad30 <aniketgaikwadhere30@gmail.com>
2025-12-13 19:46:14 +0530

    some changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,49 +1,44 @@
-// console.log("Starting backend server...");
-
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import{app,server} from './lib/socket.js';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { app, server } from "./lib/socket.js";
+import { connectDb } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import path from "path";
+
 dotenv.config();
-import {connectDb} from './lib/db.js';
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
-import path from 'path';
 
 const port = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
-
-
-// FIX: Increase body size limit for JSON and URL Encoded data
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-
-
-app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-    origin: 'http://localhost:5173',
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? true
+        : "http://localhost:5173",
     credentials: true,
-}));
+  })
+);
 
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));  
-    app.get('*', (req, res) => {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-        res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
-    }
-    );
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
 }
 
-
 server.listen(port, () => {
-  console.log(`Backend server is running on http://localhost:${port}`);
- connectDb();
+  console.log(`Backend server is running on port ${port}`);
+  connectDb();
 });
-
